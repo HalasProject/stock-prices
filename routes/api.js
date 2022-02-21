73941ce220b49c6549c5b2fd0d8af8fca5b2558d
Salah <bentayebtech@gmail.com>
2022-02-21 02:28:44 +0100

    First push

---
+++ routes/api.js
@@ -0,0 +1,55 @@
+'use strict';
+const axios = require('axios');
+const Stock = require('../models/stock');
+
+async function getStockPrice(stock, like, ip) {
+  const response = await axios.get(process.env.STOCK_API.replace('SYMBOL',stock));
+  const { close, symbol} = response.data;
+  const regex = new RegExp('^' + stock + '$', "i");    
+  let doc = await Stock.findOne({'stock': { $regex : regex } });
+  if (!doc){
+    doc = new Stock({stock: stock.trim(), likes: []});
+    if (like) doc.likes.push(ip);
+    doc.save(function (err) {
+      if (err) return handleError(err);
+      // saved!
+    });
+  } else {
+    if (like && !doc.likes.includes(ip)){
+      doc.likes.push(ip);
+    }
+    if (like == false && doc.likes.includes(ip)){
+      var index = doc.likes.indexOf(ip);
+      doc.likes.splice(index, 1);
+    }
+    doc.save(function (err) {
+      if (err) return handleError(err);
+      // saved!
+    });
+  }
+  return {
+    stock: symbol,
+    price: close,
+    likes: doc.likes ? doc.likes.length : 0
+  };
+}
+
+
+module.exports = function (app) {
+
+  app.route('/api/stock-prices')
+    .get(async function (req, res){
+      const {stock,like} = req.query;
+      let isLikeTrue = (like === 'true')
+      const ip = req.ip;
+      let result = [];
+      if (typeof(stock) === 'object' && stock.length > 1) {
+        for (const stc of stock){
+          result.push(await getStockPrice(stc, isLikeTrue, ip))
+        }
+      } else {
+        result = await getStockPrice(stock, isLikeTrue, ip)
+      };
+      res.json({stockData:result});
+  });
+};
---
+++ tests/2_functional-tests.js
@@ -0,0 +1,86 @@
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const assert = chai.assert;
+const server = require('../server');
+const { expect } = require('chai');
+const Stock = require('../models/stock');
+
+chai.use(chaiHttp);
+
+suite('Functional Tests', function() {
+
+    this.beforeAll(function (done){
+        Stock.deleteMany({}, (err) => {
+          if (err) {
+            console.error('Error deleting all documents from collection');
+            throw err;
+          }
+          done();
+        });
+      });
+  
+
+    test('Should viewing one stock',function(done) {
+        // viewing one stock: GET request to /api/stock-prices/
+        chai.request(server).get('/api/stock-prices?stock=goog').end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            assert.equal(res.body.stockData.stock, 'GOOG');
+            assert.equal(res.body.stockData.price, '2609.35');
+            done();
+        });
+    })
+   
+
+   
+    test("Should viewing one stock and liking it",function (done){
+        chai.request(server).get('/api/stock-prices?stock=goog&like=true').end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            assert.equal(res.body.stockData.stock, 'GOOG');
+            assert.equal(res.body.stockData.price, '2609.35');
+            assert.isNumber(res.body.stockData.likes);
+            done();
+        });
+    })
+  
+
+    test('Should viewing the same stock and liking it again',function (done) {
+        chai.request(server).get('/api/stock-prices?stock=goog&like=true').end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            assert.equal(res.body.stockData.stock, 'GOOG');
+            assert.equal(res.body.stockData.price, '2609.35');
+            assert.isNumber(res.body.stockData.likes);
+            done();
+        });
+    });
+ 
+
+    test("Should viewing two stocks",function (done) {
+        chai.request(server).get('/api/stock-prices?stock=goog&stock=msft').end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            assert.equal(res.body.stockData[0].stock, 'GOOG');
+            assert.equal(res.body.stockData[0].price, '2609.35');
+            assert.equal(res.body.stockData[1].stock, 'MSFT');
+            assert.equal(res.body.stockData[1].price, '287.93');
+            done();
+        });
+    });
+    
+
+    test("Should viewing two stocks and liking them",function(done) {
+        chai.request(server).get('/api/stock-prices?stock=goog&stock=msft&like=true').end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            assert.equal(res.body.stockData[0].stock, 'GOOG');
+            assert.equal(res.body.stockData[0].price, '2609.35');
+            assert.isNumber(res.body.stockData[0].likes);
+            assert.equal(res.body.stockData[1].stock, 'MSFT');
+            assert.equal(res.body.stockData[1].price, '287.93');
+            assert.isNumber(res.body.stockData[1].likes);
+            done();
+        });
+    })
+});
